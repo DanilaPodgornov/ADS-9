f2e288451096e59f0fa846c833778aab3452c116
DanilaPodgornov <116427593+DanilaPodgornov@users.noreply.github.com>
2023-06-03 22:40:31 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,6 +5,34 @@
 #include  <cstdlib>
 #include  "bst.h"
 
+bool isLetter(char c) {
+    return c >= 'a' && c <= 'z';
+}
+
 BST<std::string> makeTree(const char* filename) {
-  // поместите сюда свой код
+    BST<std::string> tree;
+    std::ifstream file(filename);
+  
+    if (!file) {
+        std::cout << "File error!" << std::endl;
+        return tree;
+    }
+
+    while (!file.eof()) {
+        char ch;
+        std::string word;
+        while (file.get(ch)) {
+            if (ch >= 'A' && ch <= 'Z') {
+                ch = tolower(ch);
+            }
+            if (isLetter(ch)) {
+                word += ch;
+            } else {
+                tree.add(word);
+                word.clear();
+            }
+        }
+    }
+    file.close();
+    return tree;
 }
