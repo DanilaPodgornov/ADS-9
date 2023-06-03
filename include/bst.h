54bf1ea0ab196b65999c654e0ab353aad9d17000
DanilaPodgornov <116427593+DanilaPodgornov@users.noreply.github.com>
2023-06-03 22:33:06 +0300

    Update bst.h

--- include/bst.h
+++ include/bst.h
@@ -1,5 +1,79 @@
 // Copyright 2021 NNTU-CS
 #ifndef INCLUDE_BST_H_
 #define INCLUDE_BST_H_
+template <typename T>
+class BST {
+ private:
+    struct Node {
+        T value;
+        int counter;
+        int depthNode;
+        Node* left;
+        Node* right;
+    };
+    Node* root;
+    int depthTree = -1;
+    Node* addNode(Node* root, T value, int depth) {
+        if (nullptr == root) {
+            root = new Node;
+            root->value = value;
+            root->counter = 1;
+            root->depthNode = depth;
+            if (depth > depthTree) {
+                depthTree = depth;
+            }
+            root->left = root->right = nullptr;
+        } else if (value < root->value) {
+            root->left = addNode(root->left, value, ++depth);
+        } else if (value > root->value) {
+            root->right = addNode(root->right, value, ++depth);
+        } else {
+            root->counter++;
+        }
+        return root;
+    }
 
+    int searchNode(Node* root, T value) {
+        if (nullptr == root) {
+            return -1;
+        } else if (value == root->value) {
+            return root->counter;
+        } else if (value < root->value) {
+            return searchNode(root->left, value);
+        } else if (value > root->value) {
+            return searchNode(root->right, value);
+        }
+    }
+
+    Node* printTree(Node* root, int level) {
+        if (nullptr == root) {
+            return nullptr;
+        }
+        std::cout << root->value << "  " << root->counter << std::endl;
+        printTree(root->left, level++);
+        printTree(root->right, level++);
+    }
+
+ public:
+    BST() {
+        root = nullptr;
+    }
+
+    void add(T value) {
+        root = addNode(root, value, 0);
+    }
+
+    void print() {
+        printTree(root, 0);
+    }
+
+    int depth() {
+        return depthTree;
+        //return root->depth;
+    }
+
+    int search(T value) {
+        return searchNode(root, value);
+    }
+};
 #endif  // INCLUDE_BST_H_
